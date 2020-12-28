89072734d06c286804d668068725a1ea2fa675da
Paul van de Ven <paul@gutstickets.org>
2020-12-28 22:07:40 +0100

    change

--- config.def.h
+++ config.def.h
@@ -65,9 +65,7 @@ static const char unknown_str[] = "n/a";
  */
 static const struct arg args[] = {
 	/* function 		format          							argument */
-	{ battery_perc,	"^c#50fa7b^ %s",        			"BAT0"         },
-	{ cpu_freq,			"^c#bbd5d4^ cpu %s",       	  NULL           },
-	{ ram_used,			"^c#d5d5bb^ mem %s",        	NULL           },
+	{ battery_perc,	"^c#50fa7b^ %s%%",        	  "BAT0"         },
 	{ wifi_essid, 	"^c#bbd5bd^ %s ",           	"wlp61s0"      },
-	{ datetime,			"^c#c8c7dc^ %s ",           	"%b %d %Y, %H" },
+	{ datetime,			"^c#c8c7dc^ %s ",           	"%b %d %Y, %R" },
 };
